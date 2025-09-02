2e01cfb1c5158e5ab612a65c6d9de8289a10aec4
Nishant Singh <166206623+NishantkSingh0@users.noreply.github.com>
2025-09-02 16:40:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from resume_utils import calculate_ats_score, Formate_Resume, process_resume
 from io import BytesIO
+import SampleResumes as sr
 
-with open("Nishant_Singh_Resume.pdf", "rb") as f:
+# or r"SampleResume/Nishant_Kumar_Resume.docx" for docx evaluation
+with open(r"SampleResume/Nishant_Singh_Resume.pdf", "rb") as f:
     buffer = BytesIO(f.read())
 
 # print(calculate_ats_score(resume_text=Formate_Resume(buffer), job_role="Data Scientist", experience_level="I"))
+# or 
+# print(calculate_ats_score(resume_text=sr.R1, job_role="HR ADMINISTRATOR/MARKETING ASSOCIATE", experience_level="II"))
 
 print(process_resume(buffer))
